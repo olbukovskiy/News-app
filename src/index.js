d1bba4189bf62b3541a10b3ffed2d6da19bdf5f4
Oleg Bukovskiy <olbukovskiy@gmail.com>
2023-01-22 00:33:58 +0200

    m

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="news-app">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
